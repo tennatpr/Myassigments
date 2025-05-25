edba98533b8f1ba79534e2ab5364718f1bf412b9
tennatpr <pravinkgk@gmail.com>
2025-05-25 13:30:11 +0530

    Add files via upload
    Day-1 Assignment

---
+++ Day-01.js
@@ -0,0 +1,32 @@
+let firstname = "Praveenkumar" // String
+console.log(firstname);
+console.log( typeof firstname );
+
+let companyname = 'cognizant' // String
+console.log(companyname);
+console.log(typeof companyname)
+
+let mobilenumber = 8940960362 // Number
+console.log(mobilenumber);
+console.log(typeof mobilenumber);
+
+let isautomation = true // Boolean
+console.log(isautomation)
+console.log(typeof isautomation);
+
+let hasplaywright;// undefined
+console.log(hasplaywright);
+console.log(typeof hasplaywright);
+
+let value = null // Object
+console.log(value);
+console.log(typeof value);
+
+
+
+
+
+
+
+
+
